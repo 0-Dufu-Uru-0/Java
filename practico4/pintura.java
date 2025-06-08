fef8ae365246e11063a91d2b9c9d0f63a20a975e
Matias <81599393+Mr-Saitam@users.noreply.github.com>
2025-06-07 22:08:47 -0300

    Update pintura.java
    Agregue el modificador: public
    Eliminado atributo redundante: valor (ahora heredado)
    Corregido constructor: parámetros mapeados correctamente
    Implementado método mostrar() con @Override
    Formato de salida mejorado

--- practico4/pintura.java
+++ practico4/pintura.java
@@ -1,23 +1,13 @@
 package practico4;
 
-class pintura extends obraarte{
-		
-		private int valor;
-	    
-		public pintura(String autor, String textura, int numcolores) {
-			
-		super(autor, textura, numcolores);
-			
-		this.valor = valor;
-
-		}
-
-		public int getValor() {
-			return valor;
-		}
-
-		public void setValor(int valor) {
-			this.valor = valor;
-		}
+public class Pintura extends ObraArte {
+    
+    public Pintura(String autor, String textura, int numColores) {
+        super(autor, textura, numColores);
+    }
+    
+    @Override
+    public String mostrar() {
+        return "Autor: " + getAutor() + " | Textura: " + getCaracteristica() + " | Colores: " + getValor();
+    }
 }
-	
