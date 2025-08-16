c5fb55caf818312df3a176b16172f861f885f48d
0-Dufu-Uru-0 <211916925+0-Dufu-Uru-0@users.noreply.github.com>
2025-08-16 16:45:06 -0300

    Add files via upload

---
+++ Ventanas/Ventana4/gestor.java
@@ -0,0 +1,15 @@
+package MiPrimeraVentana;
+
+public class gestor {
+
+	public static void main(String[] args) {
+		
+		
+
+		
+		
+      ventana ventana =  new ventana();
+      ventana.setVisible(true);
+      
+}
+}
